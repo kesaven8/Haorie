d68002cdb8429606ec0bdb9c6671579730606408
kesaven8 <kesavenvulliamay@gmail.com>
2019-01-28 18:07:15 +0800

    Added Main menu Added Sign Up page

--- app/src/main/java/com/example/kesavenvulliamay/healthapplication/MainActivity.java
+++ app/src/main/java/com/example/kesavenvulliamay/healthapplication/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.kesavenvulliamay.healthapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Log_In(View view){
+
+        intent = new Intent(this,MainMenu.class);
+        startActivity(intent);
+
+    }
+
+    // function to start activity for sign up
+    public void SignUp(View view){
+
+        intent = new Intent(this,SignUp.class);
+        startActivity(intent);
+
     }
 }
