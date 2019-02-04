0226f7ba468a57d338a7bfd605b40fecf967049f
kesaven8 <kesavenvulliamay@gmail.com>
2019-02-04 19:27:51 +0800

    Change icons Added Activity Profile

--- app/src/main/java/com/example/kesavenvulliamay/healthapplication/MainActivity.java
+++ app/src/main/java/com/example/kesavenvulliamay/healthapplication/MainActivity.java
@@ -3,16 +3,23 @@ package com.example.kesavenvulliamay.healthapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private Intent intent;
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
     //start the main menu
@@ -28,4 +35,6 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+
 }
--- app/src/main/java/com/example/kesavenvulliamay/healthapplication/MainMenu.java
+++ app/src/main/java/com/example/kesavenvulliamay/healthapplication/MainMenu.java
@@ -1,11 +1,14 @@
 package com.example.kesavenvulliamay.healthapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 public class MainMenu extends AppCompatActivity {
+
+    private Intent intent;// used to start new activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +19,12 @@ public class MainMenu extends AppCompatActivity {
     public void Test(View view){
 
         Toast.makeText(this,"i am alive",Toast.LENGTH_LONG).show();
+    }
+
+    public void Profile(View view){
+
+        intent = new Intent(this,Profile.class);
+        startActivity(intent);
+
     }
 }
